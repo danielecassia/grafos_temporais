bf692f2a910efc67b107c0a888d29f78550d0817
Daniele-Cassia <danielecassia9@gmail.com>
2024-07-23 15:27:25 -0300

    leitura dos dados

---
+++ src/main.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;  // Uso do namespace std para evitar a repetição de std::
+
+#define all(X) begin(X), end(X)
+
+struct Vertice {
+    int v;  // vertice(vila) que estou ligado
+    int a;  // final;
+    int l;  // tempo;
+    int c;  // custo;
+};
+
+// Define um struct para representar o grafo
+typedef vector<Vertice> Grafo;
+
+
+int main() {
+  int N, M;
+  cin >> N >> M;  // Lê o número de vila (N) e o número de conexões (M)
+  cout<< N << " | " << M<<endl;  // Lê o número de vila (N) e o número de conexões (M)
+
+  Grafo g;
+  g.resize(N);
+
+  for (int i = 0; i < M; i++) {  // Itera sobre todos os vértices
+    int u;
+    // faz leituras
+    cin >> u;
+    cin >> g[u].v >> g[u].a >> g[u].l >> g[u].c;
+  }
+
+  //  imprime o grafo criado
+  for (int i = 1; i < N+1; i++) {
+    cout << "Conexão entre " << i << " e " << g[i].v<<endl;
+    cout << "Ano final da construção: "<< g[i].a<<endl;
+    cout << "Tempo de travessia: "<< g[i].l<<endl;
+    cout << "Custo da comstrução: "<< g[i].c<<endl;
+    cout << "-----------------------------------------------------------";
+
+    cout << endl;
+  }
+
+  return 0;  // Retorna 0 indicando que o programa terminou com sucesso
+
+}
