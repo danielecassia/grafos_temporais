22b164158a1a7c55113505ef66de0adda56f2e4c
Daniele-Cassia <danielecassia9@gmail.com>
2024-07-23 15:38:00 -0300

    format document

--- src/main.cpp
+++ src/main.cpp
@@ -15,31 +15,31 @@ typedef vector<Conexao> Grafo;
 
 
 int main() {
-  int N, M;
-  cin >> N >> M;  // Lê o número de vila (N) e o número de conexões (M)
-//   cout<< N << " | " << M<<endl;  // Lê o número de vila (N) e o número de conexões (M)
+    int N, M;
+    cin >> N >> M;  // Lê o número de vila (N) e o número de conexões (M)
+    // cout<< N << " | " << M<<endl;  // Lê o número de vila (N) e o número de conexões (M)
 
-  Grafo g;
-  g.resize(N);
+    Grafo g;
+    g.resize(N);
 
-  for (int i = 0; i < M; i++) {  // Itera sobre todos os vértices
-    int u;
-    // faz leituras
-    cin >> u;
-    cin >> g[u].v >> g[u].a >> g[u].l >> g[u].c;
-  }
+    for (int i = 0; i < M; i++) {  // Itera sobre todos os vértices
+        int u;
+        // faz leituras
+        cin >> u;
+        cin >> g[u].v >> g[u].a >> g[u].l >> g[u].c;
+    }
 
-  //  imprime o grafo criado
-  for (int i = 1; i <= N; i++) {
-    cout << "Conexão entre " << i << " e " << g[i].v<<endl;
-    cout << "Ano final da construção: "<< g[i].a<<endl;
-    cout << "Tempo de travessia: "<< g[i].l<<endl;
-    cout << "Custo da comstrução: "<< g[i].c<<endl;
-    cout << "-----------------------------------------------------------";
+    //  imprime o grafo criado
+    for (int i = 1; i <= N; i++) {
+        cout << "Conexão entre " << i << " e " << g[i].v<<endl;
+        cout << "Ano final da construção: "<< g[i].a<<endl;
+        cout << "Tempo de travessia: "<< g[i].l<<endl;
+        cout << "Custo da comstrução: "<< g[i].c<<endl;
+        cout << "-----------------------------------------------------------";
 
-    cout << endl;
-  }
+        cout << endl;
+    }
 
-  return 0;  // Retorna 0 indicando que o programa terminou com sucesso
+    return 0;  // Retorna 0 indicando que o programa terminou com sucesso
 
 }
